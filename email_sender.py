434e4e60c3142c4f430baff996ce436c94526717
emnankhili <131199273+emnankhili@users.noreply.github.com>
2025-05-13 10:16:59 +0100

    email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -32,4 +32,5 @@ def send_email():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+     port = int(os.environ.get("PORT", 5000))  # Use the PORT variable or fallback to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
